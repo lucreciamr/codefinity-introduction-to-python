9e71a3c8a9e917070150d877fe4c35ec7ce595d9
lucreciamr <>
2026-03-04 06:24:21 +0000

    List copying

---
+++ functions/list_copying/main.py
@@ -0,0 +1,15 @@
+
+def apply_discount(prices):
+    prices_copy = prices.copy()
+    for index in range(len(prices_copy)):
+        if prices_copy[index] > 2.00:
+            prices_copy[index] *= 0.90  #the same as saying price_copy[index] = .90*price_copy[index]
+    return prices_copy 
+    
+# List of product prices
+product_prices = [1.50, 2.50, 3.00, 0.99, 2.30]
+
+# Call the function and store the updated prices
+updated_prices = apply_discount(product_prices)
+
+print('Updated product prices: ', updated_prices)
